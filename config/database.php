bd5fcc0088a9ba8436587f5824bd07ee73381f29
Stephan Vargas Schebesta <nbr920@gmail.com>
2016-11-21 00:46:55 +0000

    funcionando en cloud9

--- config/database.php
+++ config/database.php
@@ -55,9 +55,8 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'facturacion'),
+            'username' => env('DB_USERNAME', 'heroseven'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
